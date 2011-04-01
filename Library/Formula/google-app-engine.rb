c582f8422829a688fee50a3ed9a0452372454ba6
Sheng Ning <shengning@gmail.com>
2011-04-01 09:48:33 +0800

    update to 1.4.3

--- Library/Formula/google-app-engine.rb
+++ Library/Formula/google-app-engine.rb
@@ -1,16 +1,9 @@
 require 'formula'
 
-<<<<<<< HEAD
 class GoogleAppEngine < Formula
   url 'http://googleappengine.googlecode.com/files/google_appengine_1.4.3.zip'
   homepage 'http://code.google.com/appengine/'
   sha1 'c52c3ae81dcf85534bf2fd86ea76039c9f9abae8'
-=======
-class GoogleAppEngine <Formula
-  url 'http://googleappengine.googlecode.com/files/google_appengine_1.4.2.zip'
-  homepage 'http://code.google.com/appengine/'
-  sha1 '132d50710b1337169981cd78914d36df29aea722'
->>>>>>> db742a1110bf4bd9e17dd3f71ebcb1ac1d2b046d
 
   def install
     cd '..'
